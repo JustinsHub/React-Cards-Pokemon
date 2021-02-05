4412d51ba9e9fbd9425c283745e05b36c56d45b2
Justin Z <contactjustinzamora@gmail.com>
2021-02-04 19:22:03 -0800

    implemented useToggle custom hook

---
+++ src/PlayingCard.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import backOfCard from "./back.png";
+import useToggle from './hooks/useToggle'
+import "./PlayingCard.css"
+
+/* Renders a single playing card. */
+const PlayingCard = ({ front, back = backOfCard }) => {
+  const [isFacingUp, setIsFacingUp] = useToggle()
+
+  return (
+    <img
+      src={isFacingUp ? front : back}
+      alt="playing card"
+      onClick={setIsFacingUp}
+      className="PlayingCard Card"
+    />
+  );
+}
+
+export default PlayingCard;
---
+++ src/hooks/useToggle.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+const useToggle = (initial = true) => {
+    const [state, setState] = useState(initial)
+    const stateChange = () => {
+        setState(state => !state)
+    }
+    return [state, stateChange]
+}
+
+export default useToggle
